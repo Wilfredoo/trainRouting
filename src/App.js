d9cee92d250e5d947a0c225a000d5fe6ef0d0407
Wilfredo <casas.farach@gmail.com>
2020-10-20 00:15:37 +0200

    ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// INSTRUCTIONS
+// Using react-router-dom, link all the components togethr according to this structure
+// Trains
+  // Ubahn
+    // U6
+    // U7
+    // U8
+  // Sbahn
+    // S1
+    // S2
+    // S41
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route component={Trains} path="/" exact />
-        <Route component={Ubahn} path="/Ubahn" exact />
-        <Route component={SBahn} path="/Sbahn" exact />
-        <Route component={U6} path="/Ubahn/U6" />
-        <Route component={U7} path="/Ubahn/U7" />
-        <Route component={U8} path="/Ubahn/U8" />
-        <Route component={S41} path="/Sbahn/S41" />
-        <Route component={S1} path="/Sbahn/S1" />
-        <Route component={S2} path="/Sbahn/S2" />
-
-
-      </div>
-    </Router>
+    <div className="App">
+      <Trains />
+    </div>
   );
 }
 
@@ -27,14 +26,11 @@ function Trains() {
     <div className="App">
       <h1>Trains in Berlin</h1>
       <p>
-        There is Ubahn and there is Sbahn. According to studies, the Ubahn is prettier and better.
+        There is Ubahn and there is Sbahn. According to studies, the Ubahn is
+        prettier and better.
       </p>
-      <Link to="/Ubahn">
-        <p>Ubahn</p>
-      </Link>
-      <Link to="/Sbahn">
-        <p>Sbahn</p>
-      </Link>
+      <p>Ubahn</p> {/*clicking here should render Ubahn component*/}
+      <p>Sbahn</p> {/*clicking here should render Sbahn component*/}
     </div>
   );
 }
@@ -48,15 +44,9 @@ function Ubahn() {
         Germany.[1] In 2006, travel on the U-Bahn was equivalent to 122.2
         million km (76 million mi) of car journeys.[4]
       </p>
-      <Link to="/Ubahn/U6">
-        <p>U6</p>
-      </Link>
-      <Link to="/Ubahn/U7">
-        <p>U7</p>
-      </Link>
-      <Link to="/Ubahn/U8">
-        <p>U8</p>
-      </Link>
+      <p>U6</p> {/*clicking here should render U6 component*/}
+      <p>U7</p> {/*clicking here should render U7 component*/}
+      <p>U8</p> {/*clicking here should render U8 component*/}
     </div>
   );
 }
@@ -119,17 +109,14 @@ function SBahn() {
     <div className="App">
       <h1>Sbahn</h1>
       <p>
-      The Berlin S-Bahn is a rapid transit railway system in and around Berlin, the capital city of Germany. It has been in operation under this name since December 1930, having been previously called the special tariff area Berliner Stadt-, Ring- und Vorortbahnen.
+        The Berlin S-Bahn is a rapid transit railway system in and around
+        Berlin, the capital city of Germany. It has been in operation under this
+        name since December 1930, having been previously called the special
+        tariff area Berliner Stadt-, Ring- und Vorortbahnen.
       </p>
-      <Link to="/Sbahn/S1">
-        <p>S1</p>
-      </Link>
-      <Link to="/Sbahn/S2">
-        <p>S2</p>
-      </Link>
-      <Link to="/Sbahn/S41">
-        <p>S41 (Ring Bahn)</p>
-      </Link>
+      <p>S1</p> {/*clicking here should render S1 component*/}
+      <p>S2</p> {/*clicking here should render S2 component*/}
+      <p>S41 (Ring Bahn)</p> {/*clicking here should render S41 component*/}
     </div>
   );
 }
@@ -139,7 +126,9 @@ function S1() {
     <div className="App">
       <h1>S1</h1>
       <p>
-      The S1 is a line on the Berlin S-Bahn. It operates over: the Prussian Northern Railway, opened on 10 July 1877 and electrified in 1925, a short section of the Berlin-Szczecin railway
+        The S1 is a line on the Berlin S-Bahn. It operates over: the Prussian
+        Northern Railway, opened on 10 July 1877 and electrified in 1925, a
+        short section of the Berlin-Szczecin railway
       </p>
       <img
         src={
@@ -155,7 +144,9 @@ function S2() {
     <div className="App">
       <h1>S2</h1>
       <p>
-      S2 is a line on the Berlin S-Bahn.[1] It operates from Bernau to Blankenfelde      </p>
+        S2 is a line on the Berlin S-Bahn.[1] It operates from Bernau to
+        Blankenfelde{" "}
+      </p>
       <img
         src={
           "https://www.bahnbilder.de/bilder/berlin-02112008-s2-nach-bernau-241113.jpg"
@@ -170,12 +161,11 @@ function S41() {
     <div className="App">
       <h1>S41</h1>
       <p>
-      The S41 is a ring line that's approximately 37 km in length and runs clockwise stopping at stations: Südkreuz, Westkreuz, Gesundbrunnen, Ostkreuz
-</p>      <img
-        src={
-          "https://i.ytimg.com/vi/SdxN5jSxbX4/maxresdefault.jpg"
-        }
-      />
+        The S41 is a ring line that's approximately 37 km in length and runs
+        clockwise stopping at stations: Südkreuz, Westkreuz, Gesundbrunnen,
+        Ostkreuz
+      </p>{" "}
+      <img src={"https://i.ytimg.com/vi/SdxN5jSxbX4/maxresdefault.jpg"} />
     </div>
   );
 }
