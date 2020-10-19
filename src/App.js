02db6914dcdd0778d58a364e3f2d44b75806c21c
Wilfredo <casas.farach@gmail.com>
2020-10-19 17:00:32 +0200

    ubahn text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ubahn</h1>
+      <p>
+        The Berlin U-Bahn is the most extensive underground network in
+        Germany.[1] In 2006, travel on the U-Bahn was equivalent to 122.2
+        million km (76 million mi) of car journeys.[4]
+      </p>
+      <h1>U6</h1>
+      <p>
+        U6 is a Berlin U-Bahn line, 19.9 km long line with 29 stations. It runs
+        in a north-south direction from the Berlin locality of Tegel in the
+        north via Friedrichstraße to Mariendorf, a locality in the southern part
+        of the city. It is a so-called large profile line.{" "}
+      </p>
+
+      <h1>U7</h1>
+      <p>
+        The U7 is a rail line on the Berlin U-Bahn. It runs completely
+        underground for a length of 31.8 kilometres through 40 stations and
+        connects Spandau, via Neukölln, to Gropiusstadt and Rudow.
+      </p>
+      <h1>U8</h1>
+      <p>
+        U8 is a line on the Berlin U-Bahn. It has 24 stations and is 18.1 km
+        long. The U8 is one of two north–south Berlin U-Bahn lines, and runs
+        from Wittenau to Neukölln via Gesundbrunnen. The original proposal was
+        for a suspended monorail like the Wuppertal Schwebebahn. Wikipedia
+      </p>
     </div>
   );
 }
